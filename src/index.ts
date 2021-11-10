7309254d5bdbd94bc1f3769a40a56c3bb869777c
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2021-11-10 00:45:44 -0300

    Remove console.log

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,6 @@ const resetOutput = document.querySelector('#reset-output') as HTMLButtonElement
 const copyOutput = document.querySelector('#copy-output') as HTMLButtonElement;
 
 function keypressListener(event: KeyboardEvent): void {
-  console.log(event);
-
   const keyCaptured = convertKeys(event.code, event.key);
   bind.setKey(keyCaptured);
   addText(keySelected, keyCaptured);
